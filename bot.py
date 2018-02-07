afc68d99bdaeb02177360920e1c0cc4d1c4e1162
Ayrton Cyril <13516019@std.stei.itb.ac.id>
2018-02-07 21:00:40 +0700

    kalo kena kapal udh greedy untuk ngancurin kapal itu

--- bot.py
+++ bot.py
@@ -15,12 +15,13 @@ def main(player_key):
     # Retrieve current game state
     with open(os.path.join(output_path, game_state_file), 'r') as f_in:
         state = json.load(f_in)
-    print(state['PlayerMap']['Owner']['Ships'])
+    # print(state)
     map_size = state['MapDimension']
     if state['Phase'] == 1:
         place_ships()
+        # berisi lokasi kemungkinan dari kapal lawan
     else:
-        fire_shot(state['OpponentMap']['Cells'])
+        search_target(state['OpponentMap']['Cells'], map_size)
 
 
 def output_shot(x, y):
@@ -31,23 +32,90 @@ def output_shot(x, y):
     pass
 
 
-def fire_shot(opponent_map): 
+def search_target(opponent_map, map_size):
     # To send through a command please pass through the following <code>,<x>,<y>
     # Possible codes: 1 - Fireshot, 0 - Do Nothing (please pass through coordinates if
     #  code 1 is your choice)
     targets = []
-    '''print('Opponent map:')
-    print(opponent_map)
-    print('Damaged cell:')
+    print(map_size)
+    hit = False
     for cell in opponent_map:
         if cell['Damaged']:
-            print(cell)
-    print('Missed cell:')
-    for cell in opponent_map:
-        if cell['Missed']:
-            print(cell)'''
-    #i = int(input("Input any num to continue..."))
-    for cell in opponent_map:
+            hit_targets = []
+            X = cell['X']
+            Y = cell['Y']
+            hit = True
+            print(X, Y)
+            if(Y == 0):
+                Ymin = False
+            else:
+                Ymin = opponent_map[(map_size * X + Y) - 1]['Damaged']
+            if(Y == map_size - 1):
+                Yplus = False
+            else:
+                Yplus = opponent_map[(map_size * X + Y) + 1]['Damaged']
+            if(X == 0):
+                Xmin = False
+            else:
+                Xmin = opponent_map[(map_size * (X - 1) + Y)]['Damaged']
+            if(X == map_size - 1):
+                Xmplus = False
+            else:
+                Xplus = opponent_map[(map_size * (X + 1) + Y)]['Damaged']
+            if(not Yplus and not Ymin and not Xplus and not Xmin):
+                if(X != map_size - 1 and not opponent_map[(map_size * (X + 1) + Y)]['Missed']):
+                    valid_cell = X + 1, Y
+                    hit_targets.append(valid_cell)
+                if (X != 0 and not opponent_map[(map_size * (X - 1) + Y)]['Missed']):
+                    valid_cell = X - 1, Y
+                    hit_targets.append(valid_cell)
+                if (Y != map_size - 1 and not opponent_map[(map_size * X + Y ) + 1]['Missed']):
+                    valid_cell = X, Y + 1
+                    hit_targets.append(valid_cell)
+                if (Y != 0 and not opponent_map[(map_size * X + Y) - 1]['Missed']):
+                    valid_cell = X, Y - 1
+                    hit_targets.append(valid_cell)
+                print("miss smua")
+                targets = hit_targets[:]
+                break
+            elif(Yplus or Ymin):
+                tempY = Y + 1
+                while(tempY < map_size and opponent_map[(map_size * X + tempY)]['Damaged']):
+                    tempY = tempY + 1
+                if(tempY != map_size and not opponent_map[(map_size * X + tempY)]['Missed']):
+                    valid_cell = X, tempY
+                    hit_targets.append(valid_cell)
+                tempY = Y - 1
+                while(tempY >= 0 and opponent_map[(map_size * X + tempY)]['Damaged']):
+                    tempY = tempY - 1
+                if(tempY !=0 and not opponent_map[(map_size * X + tempY)]['Missed']):
+                    valid_cell = X, tempY
+                    hit_targets.append(valid_cell)
+                print("miss Y")
+                if(hit_targets != []):
+                    targets = hit_targets[:]
+                    break
+                else:
+                    print("hancur")
+            elif(Xplus or Xmin):
+                tempX = X + 1
+                while(tempX < map_size and opponent_map[(map_size * tempX + Y)]['Damaged']):
+                    tempX = tempX + 1
+                if(tempX != map_size and not opponent_map[(map_size * tempX + Y)]['Missed']):
+                    valid_cell = tempX, Y
+                    hit_targets.append(valid_cell)
+                tempX = X - 1
+                while(tempX >= 0 and opponent_map[(map_size * tempX + Y)]['Damaged']):
+                    tempX = tempX - 1
+                if(tempX !=0 and not opponent_map[(map_size * tempX + Y)]['Missed']):
+                    valid_cell = tempX, Y
+                    hit_targets.append(valid_cell)
+                print("missX")
+                if(hit_targets != []):
+                    targets = hit_targets[:]
+                    break
+                else:
+                    print("hancur")
         if not cell['Damaged'] and not cell['Missed']:
             valid_cell = cell['X'], cell['Y']
             targets.append(valid_cell)
@@ -77,8 +145,10 @@ def place_ships():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('PlayerKey', nargs='?', help='Player key registered in the game')
-    parser.add_argument('WorkingDirectory', nargs='?', default=os.getcwd(), help='Directory for the current game files')
+    parser.add_argument('PlayerKey', nargs='?',
+                        help='Player key registered in the game')
+    parser.add_argument('WorkingDirectory', nargs='?', default=os.getcwd(
+    ), help='Directory for the current game files')
     args = parser.parse_args()
     assert (os.path.isdir(args.WorkingDirectory))
     output_path = args.WorkingDirectory
